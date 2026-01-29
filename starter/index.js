35a950fd76e6ed8a76a627b329f56cae3970b1f4
Cole Nelson <ctnelson2@wisc.edu>
2026-01-28 22:00:43 -0600

    s26 ice wdb2

---
+++ starter/index.js
@@ -0,0 +1,22 @@
+// This is where your JS goes!
+
+// You can fetch data from https://cs571api.cs.wisc.edu/rest/s26/ice/chili
+// When you are complete, you should also be able to fetch data from...
+//  https://cs571api.cs.wisc.edu/rest/s26/ice/pasta
+//  https://cs571api.cs.wisc.edu/rest/s26/ice/pizza
+
+let reviewNum = 0;
+const REVIEWS = [
+    "A burst of warmth and flavor in every spoonful; simple yet irresistible!",
+    "The perfect blend of spice and comfort, an easy go-to chili recipe.",
+    "Loved the hearty texture and rich taste - a new family favorite!",
+    "Quick, flavorful, and satisfying - this chili hits all the right notes!"
+];
+
+function displayReview() {
+    alert(REVIEWS[reviewNum]);
+    reviewNum = (reviewNum + 1) % REVIEWS.length;
+}
+
+// TODO Using JavaScript, update the "N" in "Easy N-Ingredient Chili"
+//      to be the number of ingredients in the chili.
